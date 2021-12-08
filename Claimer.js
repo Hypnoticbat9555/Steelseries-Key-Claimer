581e214f601bd606c5c93f437109bd380000ecda
Hypnoticbat9555 <78077099+Hypnoticbat9555@users.noreply.github.com>
2021-12-08 18:19:49 +0100

    Update Claimer.js

--- Claimer.js
+++ Claimer.js
@@ -4,7 +4,7 @@
 // @name:fr     Steelseries Claiming
 // @match       *://games.steelseries.com/giveaway/*
 // @grant       none
-// @version     1.0
+// @version     2.0
 // @license     WTFPL
 // @author      u/hypnoticbat9555
 // @description If the user is on a giveaway page on Steelseries, it clicks the claim key button. It cannot be used to skip the waiting room. It will work if you are not in the waiting room and logged in. It basically clicks the button if/when it is available, nothing more.
@@ -13,24 +13,38 @@
 // @namespace https://greasyfork.org/users/847300
 // ==/UserScript==
 
+
 // Sets the function to be executed every 15000 Milliseconds (15 seconds)
 setInterval(function(){
-  
-    if(document.getElementById("getKeyButton")) {
+  if (CheckIfNotInWaitingRoom()) {
+    clickOnKeyButton()
+  }
+}, 15000);
+
+
+// Function that clicks the "get Key" button. Reloads the page if it doesn't
+function clickOnKeyButton() {
+  if(document.getElementById("getKeyButton")) {
       
         // If the claim button exists, click it
         // Problems: If you are not logged in, this will take you to the login page -> It won't work automatically
 		    document.getElementById("getKeyButton").click();
-      
     }
-    
     else {
-      
         // If the claim button does not exist reloads the page
-        // Problems: This will reload the page even if you are in the waiting room. I don't know if this affects your place in the line. Steelseries says it does. I think that since the page already reloads it does't affect it but I don't know.
-        // To fix this: The script would need to know if you are in the waiting room or not. Because of my limited knowledge of javascript, I can't do it. If you know how you could do it, please message me on reddit
         location.reload(); 
-      
     }
-  
-}, 20000);
+}
+
+
+// This function checks if you are NOT in the waiting room 
+function CheckIfNotInWaitingRoom()
+{
+	var othergiveawaypictures = document.getElementsByClassName('css-uwwqev');
+  // Returns true if an element with the class 'css-uwwqev' exists
+  // This class is used for the pictures on the right side of the website showing the other giveaways
+  // This means being in the waiting room will return false and being on the giveaway site true
+	return othergiveawaypictures.length > 0;
+}
+
+
